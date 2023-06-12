543fe06f77e7ba9963b8966a5247e241bf2f78a1
Parixshit <87859520+Parixshit@users.noreply.github.com>
2023-06-12 13:37:44 +0530

    spes: Initialize ArrowOS
    Signed-off-by: Parixshit <87859520+Parixshit@users.noreply.github.com>

--- arrow_spes.mk
+++ arrow_spes.mk
@@ -2,22 +2,21 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from common AOSP configuration
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from common ArrowOS configuration
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
 # Device identifier
-PRODUCT_NAME := aosp_spes
+PRODUCT_NAME := arrow_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
 
+# GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# Target
-TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_USES_AOSP_RECOVERY := true
+# Fingerprint
+BUILD_FINGERPRINT := Redmi/spes/spes:12/SKQ1.211103.001/V13.0.6.0.SGKMIXM:user/release-keys
